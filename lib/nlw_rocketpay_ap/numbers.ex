a61327641ddc6fbd5f017a787df8dd580f9f5237
Allan Presotto <allanpresottoifpr@gmail.com>
2021-02-22 10:52:47 -0300

    feat: response handle

--- lib/nlw_rocketpay_ap/numbers.ex
+++ lib/nlw_rocketpay_ap/numbers.ex
@@ -11,6 +11,6 @@ defmodule NlwRocketpayAp.Numbers do
     |> Enum.sum()
     {:ok, %{result: result}}
   end
-  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
+  defp handle_file({:error, _reason}), do: {:error, %{ message: "Invalid file!" }}
 
 end
--- lib/nlw_rocketpay_ap_web/controllers/welcome_controller.ex
+++ lib/nlw_rocketpay_ap_web/controllers/welcome_controller.ex
@@ -1,8 +1,22 @@
 defmodule NlwRocketpayApWeb.WelcomeController do
   use NlwRocketpayApWeb, :controller
+  alias NlwRocketpayAp.Numbers
 
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+  end
 
-  def index(conn, _params) do
-    text(conn, "Welcome to the Rocketpay API!")
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to Rocketpay API. Here is your number #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(reason)
   end
 end
