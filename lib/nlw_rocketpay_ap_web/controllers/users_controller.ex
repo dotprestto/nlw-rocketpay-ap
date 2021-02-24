04f0896adf68be1b21b3e7feded664f95e52d129
Allan Presotto <allanpresottoifpr@gmail.com>
2021-02-23 23:36:06 -0300

    feat: user controller, view and route

---
+++ lib/nlw_rocketpay_ap_web/controllers/users_controller.ex
@@ -0,0 +1,22 @@
+defmodule NlwRocketpayApWeb.UsersController do
+  use NlwRocketpayApWeb, :controller
+  alias NlwRocketpayAp.User
+
+  def create(conn, params) do
+    params
+    |> NlwRocketpayAp.create_user()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %User{} = user}, conn) do
+    conn
+    |> put_status(:created)
+    |> render("create.json", user: user)
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(reason)
+  end
+end
