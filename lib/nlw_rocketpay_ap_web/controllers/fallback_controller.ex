c04b1d077d00f3dfb74468dc62460eedfe760fef
Allan Presotto <allanpresottoifpr@gmail.com>
2021-02-26 18:19:14 -0300

    feat: fallback controller

---
+++ lib/nlw_rocketpay_ap_web/controllers/fallback_controller.ex
@@ -0,0 +1,10 @@
+defmodule NlwRocketpayApWeb.FallbackController do
+  use NlwRocketpayApWeb, :controller
+
+  def call(conn, {:error, result}) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(NlwRocketpayApWeb.ErrorView)
+    |> render("400.json", result: result)
+  end
+end
--- lib/nlw_rocketpay_ap_web/controllers/users_controller.ex
+++ lib/nlw_rocketpay_ap_web/controllers/users_controller.ex
@@ -2,22 +2,14 @@ defmodule NlwRocketpayApWeb.UsersController do
   use NlwRocketpayApWeb, :controller
   alias NlwRocketpayAp.User
 
+  action_fallback NlwRocketpayApWeb.FallbackController
+
   def create(conn, params) do
-    params
-    |> NlwRocketpayAp.create_user()
-    |> handle_response(conn)
-  end
+    with {:ok, %User{} = user} <- NlwRocketpayAp.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
 
-  defp handle_response({:ok, %User{} = user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render("create.json", user: user)
-  end
-
-  defp handle_response({:error, result}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(NlwRocketpayApWeb.ErrorView)
-    |> render("400.json", result: result)
   end
 end
